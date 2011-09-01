724ad5cc5d4e85f49c4b0444d7753e50004bb287
Stian Liknes <stianlik@gmail.com>
2011-09-01 23:58:35 +0200

    Test "framework" for timetable generator

--- doc/timetables/tools/combine_test/test.sh
+++ doc/timetables/tools/combine_test/test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-../combine.sh > actual.csv
-diff -I "Date*" expected.csv actual.csv
+for dir in `ls | grep ^test | tr "\n" " "`; do
+	if [ "`file $dir | sed 's/^.*: //'`" != "directory" ]; then continue; fi
 
-if [ $? -eq 0 ]; then
-	echo -e "\e[1;32mSUCCESS\e[0m"
-else
-	echo -e "\e[1;31m FAILURE\e[0m"
-fi;
+	cd $dir
+	../../combine.sh > actual.csv
+	diff -I "Date*" expected.csv actual.csv
 
-rm actual.csv
+	if [ $? -eq 0 ]; then
+		echo -e "$dir: \e[1;32mSUCCESS\e[0m"
+	else
+		echo -e "$dir: \e[1;31m FAILURE\e[0m"
+	fi;
+
+	rm actual.csv
+	cd ../
+done
