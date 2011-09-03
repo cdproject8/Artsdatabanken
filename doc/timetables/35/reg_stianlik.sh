bba57e936f7373f773674268ae369ed00c8d424a
Stian Liknes <stianlik@gmail.com>
2011-09-03 12:16:43 +0200

    Updated timetable

--- doc/timetables/35/reg_stianlik.sh
+++ doc/timetables/35/reg_stianlik.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-reg_pre_study=30
-reg_requirement=29
-reg_implementation=0
+let reg_pre_study=6+7+8+5 # mon + tue + wed + thu + fri
+let reg_requirement=0
+let reg_field_study=0
+let reg_implementation=0
--- doc/timetables/template/reg_yourname.sh
+++ doc/timetables/template/reg_yourname.sh
@@ -15,4 +15,5 @@
 
 let reg_pre_study=0
 let reg_requirement=0
+let reg_field_study=0
 let reg_implementation=0
