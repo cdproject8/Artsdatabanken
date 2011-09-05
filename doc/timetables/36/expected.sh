bfe0eadf5d93b76267bd7ed352e56a75afe62cf2
Stian Liknes <stianlik@gmail.com>
2011-09-05 16:13:35 +0200

    Created timetables for week 36

---
+++ doc/timetables/36/expected.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+let expected_pre_study=8*7
+let expected_field_study=2*7
+let expected_requirement=8*7
+let expected_implementation=6*7
---
+++ doc/timetables/36/reg_stianlik.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+let reg_pre_study=4
+let reg_requirement=0
+let reg_field_study=2
+let reg_implementation=0
