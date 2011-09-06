42c0875aa8ed1e85ffba7294b1f656399038e559
Stian Liknes <stianlik@gmail.com>
2011-09-06 09:40:16 +0200

    Added activity

--- doc/timetables/35/reg_aokaado.sh
+++ doc/timetables/35/reg_aokaado.sh
@@ -5,3 +5,4 @@ let reg_pre_study=0+4+5+3+4 # mon + tue + wed + thu + fri
 let reg_requirement=0
 let reg_field_study=0
 let reg_implementation=0
+let reg_lectures=0
--- doc/timetables/36/reg_dagingaa.sh
+++ doc/timetables/36/reg_dagingaa.sh
@@ -5,3 +5,4 @@ let reg_pre_study=0
 let reg_requirement=0
 let reg_field_study=0
 let reg_implementation=0
+let reg_lectures=0
