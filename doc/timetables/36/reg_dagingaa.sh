6a79b48747789a34e08f7247d808b8bf3bebe1cd
dagingaa <daginge@gmail.com>
2011-09-06 12:06:03 +0200

    Modified my timetable for week 36

--- doc/timetables/36/reg_dagingaa.sh
+++ doc/timetables/36/reg_dagingaa.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-let reg_administrative=0
-let reg_pre_study=0
+# Mon Tue Wed Thu Fri
+let reg_administrative=4
+let reg_pre_study=1
 let reg_requirement=0
-let reg_field_study=0
+let reg_field_study=2
 let reg_implementation=0
-let reg_lectures=0
+let reg_lectures=2+3
