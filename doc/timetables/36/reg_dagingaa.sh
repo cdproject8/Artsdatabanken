c4eeb0d8c3209e317a28c9f5e0afb61f6565e722
dagingaa <daginge@gmail.com>
2011-09-08 09:19:25 +0200

    Modified timetable registration

--- doc/timetables/36/reg_dagingaa.sh
+++ doc/timetables/36/reg_dagingaa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Mon Tue Wed Thu Fri
-let reg_administrative=4+2
-let reg_pre_study=1+3
-let reg_requirement=0+0
-let reg_field_study=2+0
-let reg_implementation=0+0
-let reg_lectures=2+2
+let reg_administrative=4+2+6
+let reg_pre_study=1+3+0
+let reg_requirement=0+0+0
+let reg_field_study=2+0+0
+let reg_implementation=0+0+0
+let reg_lectures=2+2+0
