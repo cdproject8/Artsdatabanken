6f104a3f4338a7bde775976df1b9799805187913
Stian Liknes <stianlik@gmail.com>
2011-09-09 00:44:28 +0300

    Timetable

--- doc/timetables/36/reg_stianlik.sh
+++ doc/timetables/36/reg_stianlik.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-let reg_administrative=2+1+3
-let reg_pre_study=3+3+2
-let reg_requirement=0+0+0
-let reg_field_study=0+0+2
-let reg_implementation=0+0+1 # Eclipse setup
-let reg_lectures=0+2+0
+let reg_administrative=2+1+3+1
+let reg_pre_study=3+3+2+3
+let reg_requirement=0+0+0+0
+let reg_field_study=0+0+2+0
+let reg_implementation=0+0+1+1 # Eclipse setup
+let reg_lectures=0+2+0+0
