d2732747d12c38e7af1db678ea04c576533c3133
Muhsin Gunaydin <gunaydim@gmail.com>
2011-09-12 09:40:28 +0200

    added timetable for week 36

--- doc/timetables/35/reg_muhsin89.sh
+++ doc/timetables/35/reg_muhsin89.sh
@@ -3,6 +3,6 @@
 let reg_administrative=0
 let reg_pre_study=0+7+8+8+7 # mon + tue + wed + thu + fri
 let reg_requirement=0
-let reg_field_study=0
+let reg_field_study=2
 let reg_implementation=0
 let reg_lectures=0
