fed7e3bd754b873e06c269c74749afe2d676a0e0
Muhsin Gunaydin <gunaydim@gmail.com>
2011-09-12 09:37:18 +0200

    timetables

--- doc/timetables/35/reg_muhsin89.sh
+++ doc/timetables/35/reg_muhsin89.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 let reg_administrative=0
-let reg_pre_study=0+2+3+3+2 # mon + tue + wed + thu + fri
+let reg_pre_study=0+7+8+8+7 # mon + tue + wed + thu + fri
 let reg_requirement=0
 let reg_field_study=0
 let reg_implementation=0
