955af452c82c50c7228d0b3e6a44c86501ffc680
Dag-Inge Aas <daginge@gmail.com>
2011-09-14 13:33:14 +0200

    Modified timetable

--- doc/timetables/37/reg_dagingaa.sh
+++ doc/timetables/37/reg_dagingaa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Mon Tue Wed Thu Fri
-let reg_administrative=0
+let reg_administrative=5+5+5
 let reg_pre_study=0
 let reg_requirement=0
 let reg_field_study=0
-let reg_implementation=0
+let reg_implementation=2
 let reg_lectures=0
