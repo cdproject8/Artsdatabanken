a46b083f525a11796f0a703a38f65cc10d719add
Andreas Skomedal <aokaado@gmail.com>
2011-09-19 12:57:02 +0200

    some modfications to add spec

--- src/Artsdatabanken/assets/www/js/main.js
+++ src/Artsdatabanken/assets/www/js/main.js
@@ -1,5 +1,8 @@
+//GLOBAL VARS
+var species_count=1;
+
 // Init jQuery mobile
-$(document).bind("mobileinit", function() {
+$(document).bind("mobileinit", function() {
 	// jQuery mobile configuration
 	$.extend($.mobile, {
 		// Config..
@@ -14,5 +17,21 @@ $(document).bind("mobileinit", function() {
 //phonegap replacement function for $(document).ready
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
+}
 
+function zero_pad(number, length) {
+    var num = number+"";
+    while (num.length < length) {
+        num = "0" + num;
+    }
+    return num;
+}
+
+function add_species(){
+	species_count++;
+	var new_spec = $('#species_row').clone()
+	$('input[id=spe01]', new_spec).attr("id","spe"+zero_pad(species_count,2));
+	$('input[id=spc01]', new_spec).attr("id","spc"+zero_pad(species_count,2));
+	
+	new_spec.appendTo('#observation_form');
 }
