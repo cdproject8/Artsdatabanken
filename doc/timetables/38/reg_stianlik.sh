50828222ebf8838ffc221547da3b3863447fd31c
Stian Liknes <stianlik@gmail.com>
2011-09-21 15:00:57 +0200

    updated timetable

--- doc/timetables/38/reg_stianlik.sh
+++ doc/timetables/38/reg_stianlik.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-let reg_administrative=3
+let reg_administrative=3+1
 let reg_pre_study=1
 let reg_requirement=0
 let reg_field_study=0
 let reg_implementation=5
 let reg_lectures=0
-let reg_documentation=0
+let reg_documentation=0+3
 let reg_testing=0
