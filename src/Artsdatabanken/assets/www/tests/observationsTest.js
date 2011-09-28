16130c9622c7d071c7647d4b295374524053491c
Andreas Skomedal <aokaado@gmail.com>
2011-09-28 14:11:08 +0200

    rewrote code framework

--- src/Artsdatabanken/assets/www/tests/observationsTest.js
+++ src/Artsdatabanken/assets/www/tests/observationsTest.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 	module("Module zeroPadding");
 
-	test("pad 1 to 01", function() {
+	test("pad 1,2 to 01", function() {
 		equal("01", zero_pad(1,2));
 	});
-	test("pad 10 to 10", function() {
+	test("pad 10,2 to 10", function() {
 		equal("10", zero_pad(10,2));
 	});
+	test("pad 0,3 to 000", function() {
+		equal("000", zero_pad(0,3));
+	});
+	test("pad 1,3 to 001", function() {
+		equal("001", zero_pad(1,3));
+	});
+
 
 });
