57427515e0374a094d2a19bf7b89194f5d20cb7e
Anders Rye <anders.rye@gmail.com>
2011-09-29 15:19:47 +0200

    more, smoother camera test stuff

--- src/Artsdatabanken/assets/www/js/Camera.js
+++ src/Artsdatabanken/assets/www/js/Camera.js
@@ -1,14 +1,37 @@
 
-var camSuccess = function(image) {
+var camSuccess = function(uri) {
 	//do something real here, for now just replaces the image on the page
 	img = document.getElementById('testimg');
-	img.src = "data:image/jpeg;base64," + image;
+//	img.src = "data:image/jpeg;base64," + image;
+	img.src = uri;
 };
 
 var camError = function(error) {
 	alert(error);
 }
 
-function takePicture() {
-	navigator.camera.getPicture(camSuccess, camError, {quality: 50, targetWidth: 640, targetHeight: 480})
+function takePictureAsData() {
+	navigator.camera.getPicture(camSuccess, camError, {
+		quality: 90, 
+		targetWidth: 640, 
+		targetHeight: 480
+	})
+}
+
+function takePictureAsURI() {
+	navigator.camera.getPicture(camSuccess, camError, {
+		quality: 90, 
+		targetWidth: 640, 
+		targetHeight: 480, 
+		destinationType: navigator.camera.DestinationType.FILE_URI
+	})
+}
+
+function retrievePicture() {
+	navigator.camera.getPicture(camSuccess, camError, {
+		targetWidth: 640,
+		targetHeight:480,
+		destinationType: navigator.camera.DestinationType.FILE_URI,
+		sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+	})
 }
