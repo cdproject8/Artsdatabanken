9a5a23923eed37b7e436998f40e602911bd035d5
Andreas Skomedal <aokaado@gmail.com>
2011-09-29 09:37:57 +0200

    test index

--- src/Artsdatabanken/assets/www/js/Observation.js
+++ src/Artsdatabanken/assets/www/js/Observation.js
@@ -17,7 +17,7 @@ function Observation(){
 		newSpec = new ObsSpec(this.len());
 		this.species.push(newSpec);
 		$('#observation_form').append(newSpec.speciesListHtml().trigger('create'));
-		add_autocomplete("#spe"+newSpec.id);
+		add_autocomplete("#species_row"+newSpec.id +" .ui-input-name-spec");
 	}
 	
 	this.len = function() {
