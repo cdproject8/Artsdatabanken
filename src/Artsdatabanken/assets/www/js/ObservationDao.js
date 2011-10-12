248fddcfc97ead0131c2261f776083e39cae1f8c
Stian Liknes <stianlik@gmail.com>
2011-10-12 09:36:11 +0200

    All tests are running

---
+++ src/Artsdatabanken/assets/www/js/ObservationDao.js
@@ -0,0 +1,28 @@
+VERSION = 0.2;
+function ObservationDao() {
+	db = null;
+	
+	this.connect = function() {
+		db = window.openDatabase("observations", VERSION, "ObservationsDB", 1048576);
+		return this;
+	};
+
+	this.install = function(errorCallback) {
+		db.transaction(function(tx) {
+			tx.executeSql('CREATE TABLE IF NOT EXISTS observations (obsid INTEGER PRIMARY KEY AUTOINCREMENT, location)');	
+			tx.executeSql('CREATE TABLE IF NOT EXISTS species (spcid INTEGER PRIMARY KEY AUTOINCREMENT, observation_id, species, number, sex, age, activity, time_start INTEGER, time_end INTEGER, comments)');	
+			tx.executeSql('CREATE TABLE IF NOT EXISTS test (data)');
+			
+		}, errorCallback);
+	};
+
+	this.db = function(data) {
+		if (data != null) {
+			db = data;
+		}
+		return db;
+	}
+
+	this.connect();
+	this.install();
+}
