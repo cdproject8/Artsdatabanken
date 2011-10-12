70cb482fdd62d503a1b374754b1e0f0e23ad884e
Stian Liknes <stianlik@gmail.com>
2011-10-12 13:49:25 +0200

    Database stuffs

--- src/Artsdatabanken/assets/www/js/ObservationDao.js
+++ src/Artsdatabanken/assets/www/js/ObservationDao.js
@@ -14,10 +14,19 @@ function ObservationDao() {
 
 	this.install = function(errorCallback) {
 		db.transaction(function(tx) {
-			tx.executeSql('CREATE TABLE IF NOT EXISTS observations (obsid INTEGER PRIMARY KEY AUTOINCREMENT, location)');	
-			tx.executeSql('CREATE TABLE IF NOT EXISTS species (spcid INTEGER PRIMARY KEY AUTOINCREMENT, observation_id, species, number, sex, age, activity, time_start INTEGER, time_end INTEGER, comments)');	
+			tx.executeSql('CREATE TABLE IF NOT EXISTS observations (obsid INTEGER PRIMARY KEY AUTOINCREMENT, location)');
+			tx.executeSql('CREATE TABLE IF NOT EXISTS species (spcid, observation_id, species, number, sex, age, activity, time_start INTEGER, time_end INTEGER, comments, PRIMARY KEY (spcid, observation_id))');	
 			tx.executeSql('CREATE TABLE IF NOT EXISTS test (data)');
 		}, errorCallback);
+		return me;
+	};
+	
+	this.uninstall = function(error) {
+		db.transaction(function(tx) {
+			tx.executeSql('DROP TABLE observations');
+			tx.executeSql('DROP TABLE species');	
+			tx.executeSql('DROP TABLE test');
+		}, error);
 		return me;
 	};
 
@@ -27,9 +36,49 @@ function ObservationDao() {
 		}
 		return db;
 	};
+	
+	this.saveEntry = function(entry, success, error) {
+//		var v = '';
+//		v += observation_id + ', "';
+//		v += species + '", "';
+//		v += number + '", "';
+//		v += sex + '", "';
+//		v += age + '", "';
+//		v += activity + '", ';
+//		v += time_start + ', ';
+//		v += time_end + ', "';
+//		v += comments + '"';
+//		executeQuery('INSERT INTO species VALUES (NULL,' + v + ')');
+		var query = "INSERT INTO SPECIES VALUES (?,?,?,?,?,?,?,?,?,?)";
+		var values = [
+		    entry.id,
+          	entry.observation.id,
+			entry.species_name,
+			entry.count,
+			entry.sex,
+			entry.age,
+			entry.activity,
+			entry.date_start.getTime(), 
+			entry.date_end.getTime(),
+			entry.comment
+		];
+		db.transaction(function(tx) {
+			tx.executeSql(query, values, function(tx, results) {
+				success(results.insertId);
+			}, null);
+		}, error);
+	};
+	
+	this.findEntry = function(id, success, error) {
+		db.transaction(function(tx) {
+			tx.executeSql('SELECT * FROM species WHERE spcid=' + id, [], function(tx, results) {
+				success({species_name: results.rows.item(0).species});
+			}, null)
+		}, error)
+//		success({species_name: "Big dog"});
+	};
 
 	// Constructor
-	
 	this.connect();
 	this.install();
 }
--- src/Artsdatabanken/assets/www/tests/ObservationDaoTest.js
+++ src/Artsdatabanken/assets/www/tests/ObservationDaoTest.js
@@ -1,8 +1,73 @@
 $(document).ready(function(){
 	module("ObservationDao");
 
+	function expectTable(tableName, callback) {
+		var dao = new ObservationDao();
+		dao.db().transaction(function (tx) { 
+			tx.executeSql('SELECT * FROM observations', [], function(result) {
+				ok(true, tableName + " table exists");
+				callback();
+			}, function(err) {
+				ok(false, tableName + " table missing");
+				callback();
+			});
+		});
+	}
+
 	test("should be able to connect to database", function() {
 		var dao = new ObservationDao();
 		ok(dao.connect().db().version, "Database has version property");
+	});
+
+	asyncTest("should create observations table on install", function() {
+		expect(2);
+		expectTable("observation", function() {
+			expectTable("species", function() {
+				start();
+			});
+		});
+	});
+
+	asyncTest("should be able to save and retrieve observation entry", function() {
+		var dao = new ObservationDao();
+
+		dao.uninstall();
+		dao.install();
+		expect(2);
+		var entry = {
+			observation: {id: 1},
+			id: 2,
+			species_name: "Big dog",
+			count: 12,
+			sex: "Male",
+			age: 11,
+			activity: "Sleeping",
+			date_start: new Date(1234), 
+			date_end: new Date(4321),
+			comment: "This is a comment"
+		};
+		
+		// Add entry nr 1
+		dao.saveEntry(entry, function(result) {
+			dao.findEntry(entry.id, function(result) {
+				console.log(result);
+				equals(result.species_name, entry.species_name);
+				
+				// Add entry nr 2
+				entry.id = 32;
+				entry.species_name = "Small dog";
+				dao.saveEntry(entry, function(result) {
+					dao.findEntry(entry.id, function(result) {
+						equals(result.species_name, entry.species_name);
+						start();
+					});
+				}, function(error) {
+					start();
+				});
+			});
+		}, function(error) {
+			console.log(error);
+			start();
+		});
 	});
 });
