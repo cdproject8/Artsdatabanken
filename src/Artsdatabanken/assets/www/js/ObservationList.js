2ce8334099095cd8db0a206a5e246d85d3b7e2b4
Aokaado <aokaado@gmail.com>
2011-10-13 10:57:19 +0200

    obslist w dao update

--- src/Artsdatabanken/assets/www/js/ObservationList.js
+++ src/Artsdatabanken/assets/www/js/ObservationList.js
@@ -1,23 +1,27 @@
-ObservationList = function() {
+function ObservationList(dao) {
+	console.log("init obs");
 	
-	var ObsDao = new ObservationDao();
+	var error;
 	
-	this.populateList(){
-		dao.findObservations( function(result) {
+	this.populateList = function(){
+		dao.findAllObservations( function(result) {
 			for (var i = 0; i < result.length; i++){
+				var obsId = result.item(0).observation_id;
 				var date;
 				// Note: requires an observation to have at least one entry !!
 				dao.findAllEntries({observation_id: result.item(0).id, limit: 1}, function(results){
 					date = new Date(results.item(0));
 				}, error);
 				var htmlstring = '<li>'
-							   + '	<a href="observation.html">'
-							   + '	<h3>'+result.item(i)+'</h3>'
-							   + '	<p>'+date+'</p>'
-							   + '	</a></li>';
+							   + '	<a href="observation.html" onClick= observationId='+obsId+'>'
+							   + '		<h3>'+result.item(i)+'</h3>'
+							   + '		<p>'+date+'</p>'
+							   + '	</a>'
+							   + '</li>';
 		   		$('#species_list').append(htmlstring);
 			}
 		}, error);
 		$("#species_list").listview("refresh");
 	}
+	
 }
