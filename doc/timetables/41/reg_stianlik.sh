2ee1b565abc714a493ed2a02b5bb873e2cbadb22
Stian Liknes <stianlik@gmail.com>
2011-10-13 09:19:09 +0200

    Timetable for yesterday

--- doc/timetables/41/reg_stianlik.sh
+++ doc/timetables/41/reg_stianlik.sh
@@ -2,11 +2,12 @@
 
 # mandag	= 0
 # tirsdag	= 6
+# onsdag	= 6
 let reg_administrative=1
 let reg_pre_study=0
 let reg_requirement=0
 let reg_field_study=0
-let reg_implementation=3
+let reg_implementation=3+3
 let reg_lectures=0
 let reg_documentation=0
-let reg_testing=2
+let reg_testing=2+3
