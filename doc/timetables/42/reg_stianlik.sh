7efbcedfd92996132d7a82afe9bb311deef7fd80
Stian Liknes <stianlik@gmail.com>
2011-10-21 09:38:24 +0200

    Prepared timetables for week 42 and 43. Filled in my timetable

---
+++ doc/timetables/42/reg_stianlik.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+let reg_administrative=4
+let reg_pre_study=0
+let reg_requirement=0
+let reg_field_study=0
+let reg_implementation=1
+let reg_lectures=4
+let reg_documentation=0
+let reg_testing=1
