d63bcbaf28edf0a9a283a58fa03e52b411e2d381
Aokaado <aokaado@gmail.com>
2011-11-02 13:00:39 +0100

    fixed picture?

--- src/Artsdatabanken/src/com/borismus/webintent/WebIntent.java
+++ src/Artsdatabanken/src/com/borismus/webintent/WebIntent.java
@@ -110,7 +110,7 @@ public class WebIntent extends Plugin {
 				String filePaths[] = value.split("\\*");
 				if (filePaths.length != 0) {
 					Log.d("PhoneGapLog", "fp len: "+filePaths.length); 
-					ArrayList<Uri> uris = new ArrayList<Uri>();
+					//ArrayList<Uri> uris = new ArrayList<Uri>();
 				    //convert from paths to Android friendly Parcelable Uri's
 				    for (String file : filePaths)
 				    {
@@ -121,11 +121,12 @@ public class WebIntent extends Plugin {
 				        //File fileIn = new File(realPath);
 				        //Uri u = Uri.fromFile(fileIn);
 				        Uri u = Uri.parse("file://"+realPath);
-				    	Log.d("PhoneGapLog", "uri.getpath "+u.getPath());
-				        uris.add(u);
+				    	//Log.d("PhoneGapLog", "uri.getpath "+u.getPath());
+				        //uris.add(u);
+				        i.putExtra(Intent.EXTRA_STREAM, u);
 				    }
 					
-					i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+					//i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
 				}
 			} else {
 				i.putExtra(key, value);
